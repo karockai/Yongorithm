0a6d90df5d7ed34e68738f8109f95fb4fd971ede
karockai <75655052+karockai@users.noreply.github.com>
2021-04-26 02:33:00 +0900

    최단경로 done

--- 그래프/KCMTravel.py
+++ 그래프/KCMTravel.py
@@ -1,10 +1,50 @@
 
+import heapq
+def saveRouteInfo(routes, routeInfo):
+    if (routeInfo[0] in routes):
+        route = routes[routeInfo[0]]
+        route.append([routeInfo[3], routeInfo[2], routeInfo[0], routeInfo[1]])
+        routes[routeInfo[0]] = route
+    else:
+        routes[routeInfo[0]] = [[routeInfo[3], routeInfo[2], routeInfo[0], routeInfo[1]]]
+
+def dijkstra(routes, N, M):
+    # 최단거리를 저장할 배열
+    timeTable = [[float('inf') for col in range(N+1)] for row in range(N+1)]
+
+    # fastest[i] = [[해당 노드까지의 시간, 비용]]
+    tripInfo = [[float('inf'), 0] for col in range(N+1)]
+    tripInfo[1] = [0,0]
+ 
+    readyQ = []
+    curNode = 1
+    nextRoutes = routes[curNode]
+    for nextRoute in nextRoutes:
+        heapq.heappush(readyQ, nextRoute)
+
+    while (readyQ):
+        curTrip = heapq.heappop(readyQ)
+        if (tripInfo[curTrip[3]][1] + curTrip[1] > M): continue
+        tripInfo[curTrip[3]][0] += curTrip[0]
+        tripInfo[curTrip[3]][1] += curTrip[1]
+        nextRoutes = routes[curNode]
+        for nextRoute in nextRoutes:
+            heapq.heappush(readyQ, nextRoute)
 
 def func(N, M, K):
+    # 경로 정보. {현 공항 : [[시간, 비용, 출발지, 목적지]]} 
+    routes = {}
 
-
-
-
+    # 정보를 집어 넣음
+    for k in range(K):
+        routeInfo = list(map(int,sys.stdin.readline().split()))
+        saveRouteInfo(routes, routeInfo)
+    
+    answer = float('inf')
+    answer = dijkstra(routes, N, M)
+    
+    if (answer = float('inf')): return 'Poor KCM'
+    else: return answer
 
 sys.stdin = open("KCMTravel.txt","r")
 T = int(sys.stdin.readline().rstrip())
---
+++ 그래프/최단경로.py
@@ -0,0 +1,55 @@
+import heapq
+import sys
+def inputRoutes(source, dest, point, routes):
+    # routes = {source : (point, prev, dest)}
+    if (not source in routes):
+        routes[source] = []
+    routes[source].append((point, source, dest))
+
+def dijkstra(start, routes, V):
+    # fastPath 는 start로부터 node까지의 최단 비용과 경로를 저장한다.
+    fastPath = [float('inf') for col in range(V+1)]
+    fastPath[start] = 0
+
+    visited = [False for col in range(V+1)]
+    visited[start] = True
+
+    # readyQ[0] =  start로부터 가장 가까운 미방문 Node가 들어있다.
+    readyQ = []
+    # nextRoute = (point, prev, dest)
+    nextRoutes = routes[start]
+    for nextRoute in nextRoutes:
+        if (fastPath[nextRoute[2]] < nextRoute[0]): continue
+        heapq.heappush(readyQ, (nextRoute[0], nextRoute))
+    
+    while (readyQ):
+        tmp = heapq.heappop(readyQ)
+        cost = tmp[0]
+        curRoute = tmp[1]
+        if (fastPath[curRoute[2]] < fastPath[curRoute[1]] + curRoute[0]): continue
+
+        fastPath[curRoute[2]] = cost
+
+        if (curRoute[2] in routes and visited[curRoute[2]] == False):
+            newRoutes = routes[curRoute[2]]
+            for newRoute in newRoutes:
+                
+                if (fastPath[newRoute[2]] < fastPath[curRoute[2]] + newRoute[0]): continue
+                heapq.heappush(readyQ, (fastPath[curRoute[2]] + newRoute[0], newRoute))
+            visited[curRoute[2]] = True
+
+    for i in range(1, V+1):
+        if (fastPath[i] == float('inf')): print('INF')
+        else: print(fastPath[i])
+
+# sys.stdin = open("최단경로.txt","r")
+
+V, E = map(int,sys.stdin.readline().split())
+K = int(sys.stdin.readline().rstrip())
+routes = {}
+
+for e in range(E):
+    edge = list(map(int,sys.stdin.readline().split()))
+    inputRoutes(edge[0], edge[1], edge[2], routes)
+
+dijkstra(K, routes, V)
